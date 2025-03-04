68543ce78126e697141011cdfff098d16c079d40
SimonDeVos <devos.simon@gmail.com>
2025-03-04 16:49:07 +0100

    -

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,7 @@
+import random
+
+import numpy as np
+import torch
 import yaml
 
 
@@ -24,7 +28,20 @@ def _assert_dataconfig(dataconfig, experimentconfig):
     pass
 
 def _assert_methodconfig(methodconfig):
+    # todo: implement
     pass
 
 def _assert_evaluationconfig(evaluationconfig):
+    # todo: implement
     pass
+
+
+def set_random_seed(seed=0):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
